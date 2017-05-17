70f109aa7064b5becd0b8d2d035aa3e0066a5c69
glenndewildt <glenndewildt@gmail.com>
2017-05-17 10:29:55 +0200

    New design

---
+++ BinaireRekenmachine/BinaireRekenmachine/Input/CircuitInputReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaireRekenmachine.Input
+{
+    class CircuitInputReader
+    {
+        public string ReadInputFile(String textFile) {
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(textFile))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                    return line;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return "";
+
+        }
+    }
+}
---
+++ BinaireRekenmachine/BinaireRekenmachine/Input/UserInputReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaireRekenmachine.Input
+{
+    class UserInputReader
+    {
+        public String getUserInput() {
+            return Console.ReadLine();
+        }
+    }
+}
